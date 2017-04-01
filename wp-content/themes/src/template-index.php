738cc169466051952e97196a3ce00201e4929d94
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-04-01 13:28:26 +0100

    Add JS and begun starting to install Grunt

--- wp-content/themes/src/template-index.php
+++ wp-content/themes/src/template-index.php
@@ -1,25 +1,33 @@
 <?php /* Template Name: Index Page Template */ get_header(); ?>
 
 	<main role="main" aria-label="Content">
-		<!-- section -->
-		<section>
-
-			<h1><?php the_title(); ?></h1>
-
 			<?php
 			$args = array( 'post_type' => 'fact', 'posts_per_page' => 10 );
 			$loop = new WP_Query( $args );
 			while ( $loop->have_posts() ) : $loop->the_post();
-			  echo '<div class="fact-container">';
-				  the_title();
-				  the_field('increment_value');
-				  the_field('background_colour');
-			  echo '</div>';
+				$increment = get_field('increment_value');
+				echo '<section class="fact-container" data-service="';
+				echo $increment;
+				echo '">';
+				the_title();
+				the_field('background_colour');
+				echo '<div id="demo"></div>';
+				echo '</section>';
 			endwhile;
 			?>
 
-		</section>
-		<!-- /section -->
 	</main>
+
+	<script>
+		var js_variable  = '<?php echo $increment;?>';
+
+		var interval = setInterval( increment, 1000);
+
+		function increment(){
+		    js_variable = js_variable % 360 + js_variable;
+		}
+
+		document.getElementById("demo").innerHTML = js_variable;
+	</script>
 
 <?php get_footer(); ?>
