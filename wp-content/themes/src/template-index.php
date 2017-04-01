7a57786972b15c4363e7d08df49e6fac5f23f733
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-04-01 16:22:51 +0100

    It works....?

--- wp-content/themes/src/template-index.php
+++ wp-content/themes/src/template-index.php
@@ -6,9 +6,19 @@
 			$loop = new WP_Query( $args );
 			while ( $loop->have_posts() ) : $loop->the_post();
 				$increment = get_field('increment_value');
-				echo '<section class="fact" data-service="';
+
+				echo '<section class="fact ';
+				the_title();
+				echo '" ';
+
+				echo 'id="';
+				the_title();
+				echo '" ';
+
+				echo 'data-increment="';
 				echo $increment;
 				echo '"';
+
 				echo 'style="background-color:';
 				the_field('background_colour');
 				echo '">';
@@ -19,7 +29,9 @@
 				the_post_thumbnail();
 				echo '</div>';
 
-				echo '<h2 class="fact__value" id="demo">2</h2>';
+				echo '<h2 class="fact__value" id="demo-';
+				the_title();
+				echo '">2</h2>';
 
 				echo '<h3 class="fact__strapline">';
 				the_field('intro_text');
@@ -28,30 +40,24 @@
 				echo '</div>';
 
 				echo '</section>';
+			?>
+
+			<script>
+				var js_variable  = 0;//'<?php echo $increment;?>';
+
+				var interval = setInterval( increment, 1000);
+
+				function increment(){
+					js_variable += 1;// js_variable % 360 + js_variable;
+
+					document.getElementById("demo-<?php the_title(); ?>").innerHTML = document.getElementById("<?php the_title(); ?>").getAttribute('data-increment') * js_variable;
+				}
+			</script>
+
+			<?php
 			endwhile;
 			?>
 
 	</main>
-
-	<script>
-	var js_variable  = 0;//'<?php echo $increment;?>';
-
-		var interval = setInterval( increment, 1000);
-
-		function increment(){
-		    js_variable += 1;// js_variable % 360 + js_variable;
-
-			var xhttp = new XMLHttpRequest();
-			xhttp.onreadystatechange = function() {
-			  if (this.readyState == 4 && this.status == 200) {
-			   document.getElementById("demo").innerHTML = this.responseText * js_variable;
-			  }
-			};
-			xhttp.open("GET", "/wp-content/test.php", true);
-			xhttp.send();
-		}
-
-
-	</script>
 
 <?php get_footer(); ?>
