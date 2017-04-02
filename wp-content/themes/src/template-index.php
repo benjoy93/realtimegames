7205811849d3b981e34d98867dd25cbd7a852261
JackDaviesWork <jackdavieswork@hotmail.co.uk>
2017-04-02 14:31:57 +0100

    Auto indent homepage template

--- wp-content/themes/src/template-index.php
+++ wp-content/themes/src/template-index.php
@@ -1,77 +1,76 @@
 <?php /* Template Name: Index Page Template */ get_header(); ?>
 
-	<main role="main" aria-label="Content">
+<main role="main" aria-label="Content">
 
 	<section class="fact fact--full text-color--dark">
 		<h1>Video Games in real time</h1>
-		<h5> Scroll down to see what has happened in the video game world since you landed on this page!</h5>
+		<h5>Scroll down to see what has happened in the video game world since you landed on this page!</h5>
 		<img src="<?php echo get_template_directory_uri(); ?>/img/arrow.svg">
 	</section>
 
+	<?php
+	$namea = 1;
+	$args = array( 'post_type' => 'fact', 'posts_per_page' => -1 );
+	$loop = new WP_Query( $args );
+	while ( $loop->have_posts() ) : $loop->the_post();
+	$increment = get_field('increment_value');
 
-			<?php
-			$namea = 1;
-			$args = array( 'post_type' => 'fact', 'posts_per_page' => -1 );
-			$loop = new WP_Query( $args );
-			while ( $loop->have_posts() ) : $loop->the_post();
-				$increment = get_field('increment_value');
+	echo '<section class="fact ';
+	the_title();
+	echo '" ';
 
-				echo '<section class="fact ';
-				the_title();
-				echo '" ';
+	echo 'id="';
+	the_title();
+	echo '" ';
 
-				echo 'id="';
-				the_title();
-				echo '" ';
+	echo 'data-increment="';
+	echo $increment;
+	echo '"';
 
-				echo 'data-increment="';
-				echo $increment;
-				echo '"';
+	echo 'style="background-color:';
+	the_field('background_colour');
+	echo '">';
 
-				echo 'style="background-color:';
-				the_field('background_colour');
-				echo '">';
+	echo '<div class="fact__container">';
 
-				echo '<div class="fact__container">';
+	echo '<div class="fact__icon">';
+	the_post_thumbnail();
+	echo '</div>';
 
-				echo '<div class="fact__icon">';
-				the_post_thumbnail();
-				echo '</div>';
+	echo '<h2 style="display: inline-block;" class="fact__value" id="demo-';
+	the_title();
+	echo '"></h2>';
 
-				echo '<h2 style="display: inline-block;" class="fact__value" id="demo-';
-				the_title();
-				echo '"></h2>';
+	echo '<h2 style="display: inline-block; margin-left: 10px;">';
+	the_field('increment_unit');
+	echo '</h2>';
 
-				echo '<h2 style="display: inline-block; margin-left: 10px;">';
-				the_field('increment_unit');
-				echo '</h2>';
+	echo '<h3 class="fact__strapline text-weight--normal">';
+	the_field('intro_text');
+	echo '</h3>';
 
-				echo '<h3 class="fact__strapline text-weight--normal">';
-				the_field('intro_text');
-				echo '</h3>';
+	echo '</div>';
 
-				echo '</div>';
+	echo '</section>';
+	?>
 
-				echo '</section>';
-			?>
+	<script>
+	var js_variable<?php echo $namea; ?>  = 0;//'<?php echo $increment;?>';
 
-			<script>
-				var js_variable<?php echo $namea; ?>  = 0;//'<?php echo $increment;?>';
+	var interval<?php echo $namea ?> = setInterval( increment<?php echo $namea ?>, 1000);
 
-				var interval<?php echo $namea ?> = setInterval( increment<?php echo $namea ?>, 1000);
+	function increment<?php echo $namea; ?>(){
+		js_variable<?php echo $namea; ?> += 1;// js_variable % 360 + js_variable;
 
-				function increment<?php echo $namea; ?>(){
-					js_variable<?php echo $namea; ?> += 1;// js_variable % 360 + js_variable;
+		document.getElementById("demo-<?php the_title(); ?>").innerHTML = document.getElementById("<?php the_title(); ?>").getAttribute('data-increment') * js_variable<?php echo $namea; ?>;
+	}
+	</script>
 
-					document.getElementById("demo-<?php the_title(); ?>").innerHTML = document.getElementById("<?php the_title(); ?>").getAttribute('data-increment') * js_variable<?php echo $namea; ?>;
-				}
-			</script>
+	<?php
+	$namea++;
+endwhile;
+?>
 
-			<?php
-				$namea++;
-			endwhile;
-			?>
-
-	</main>
+</main>
 
 <?php get_footer(); ?>
