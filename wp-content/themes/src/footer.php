0ef077a64697abc635915b42ec9a9358af295056
Jack-Moov2 <jackdavieswork@hotmail.co.uk>
2017-04-05 20:26:47 +0100

    Include render CSS/JS below the fold

--- wp-content/themes/src/footer.php
+++ wp-content/themes/src/footer.php
@@ -13,17 +13,11 @@
 		</div>
 		<!-- /wrapper -->
 
+		<!-- W3TC-include-css -->
+
 		<?php wp_footer(); ?>
 
-		<!-- analytics -->
-		<script>
-		(function(f,i,r,e,s,h,l){i['GoogleAnalyticsObject']=s;f[s]=f[s]||function(){
-		(f[s].q=f[s].q||[]).push(arguments)},f[s].l=1*new Date();h=i.createElement(r),
-		l=i.getElementsByTagName(r)[0];h.async=1;h.src=e;l.parentNode.insertBefore(h,l)
-		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-XXXXXXXX-XX', 'yourdomain.com');
-		ga('send', 'pageview');
-		</script>
+		<!-- W3TC-include-js-head -->
 
 	</body>
 </html>
