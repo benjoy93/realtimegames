c712890ee863b69f89afb91ac088dbf7d0aad506
Jack-Moov2 <jackdavieswork@hotmail.co.uk>
2017-04-06 16:11:31 +0100

    Allow for SVG Uploads

--- wp-content/themes/src/functions.php
+++ wp-content/themes/src/functions.php
@@ -410,7 +410,7 @@ function create_post_type_html5()
 }
 
 /*------------------------------------*\
-REMOVE WP EMOJI
+    REMOVE WP EMOJI
 \*------------------------------------*/
 
 remove_action('wp_head', 'print_emoji_detection_script', 7);
@@ -418,3 +418,41 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 
 remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+/*------------------------------------*\
+    ALLOW ANY UPLOAD OF FILES
+\*------------------------------------*/
+
+// Allow SVG
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+  global $wp_version;
+  if ( $wp_version !== '4.7.1' ) {
+     return $data;
+  }
+
+  $filetype = wp_check_filetype( $filename, $mimes );
+
+  return [
+      'ext'             => $filetype['ext'],
+      'type'            => $filetype['type'],
+      'proper_filename' => $data['proper_filename']
+  ];
+
+}, 10, 4 );
+
+function cc_mime_types( $mimes ){
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter( 'upload_mimes', 'cc_mime_types' );
+
+function fix_svg() {
+  echo '<style type="text/css">
+        .attachment-266x266, .thumbnail img {
+             width: 100% !important;
+             height: auto !important;
+        }
+        </style>';
+}
+add_action( 'admin_head', 'fix_svg' );
